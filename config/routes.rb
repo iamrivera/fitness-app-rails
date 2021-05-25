23973852ea91a984bfc994148fec59837f948a07
iamrivera <a3m3rivera@gmail.com>
2021-05-24 23:20:04 -0700

    Created the exercise controller's actions and drew the exercise routes - basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :trainers, :workouts, :users
+  resources :trainers, :workouts, :users, :exercises 
 
 end
